172a9796b6cab5f76881d3f6e567a0ae93aba27a
Robin Schmid <robinschmid@uni-muenster.de>
2018-02-13 17:11:09 +0100

    Barchart is better now
    Todo gaussian

--- src/main/java/net/rs/lamsi/general/myfreechart/EChartFactory.java
+++ src/main/java/net/rs/lamsi/general/myfreechart/EChartFactory.java
@@ -2,12 +2,16 @@ package net.rs.lamsi.general.myfreechart;
 
 import java.awt.Color;
 import java.util.function.DoubleFunction;
+import org.apache.commons.math3.analysis.function.Gaussian;
+import org.apache.commons.math3.fitting.GaussianCurveFitter;
+import org.apache.commons.math3.fitting.WeightedObservedPoints;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.StandardXYBarPainter;
 import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.Range;
 import org.jfree.data.statistics.HistogramDataset;
 import org.jfree.data.xy.XYBarDataset;
@@ -16,67 +20,111 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 public class EChartFactory {
 
-  public static JFreeChart createHistogram(double[] data, int bin) {
-    return createHistogram(data, bin, null);
+  public static JFreeChart createHistogram(double[] data, double binwidth) {
+    return createHistogram(data, binwidth, null);
   }
 
-  public static JFreeChart createHistogram(double[] data, int bin, String yAxisLabel) {
+  public static JFreeChart createHistogram(double[] data, double binwidth, String yAxisLabel) {
     Range r = getBounds(data);
-    return createHistogram(data, bin, yAxisLabel, r.getLowerBound(), r.getUpperBound(), val -> val);
+    return createHistogram(data, binwidth, yAxisLabel, r.getLowerBound(), r.getUpperBound(),
+        val -> val);
   }
 
-  public static JFreeChart createHistogram(double[] data, int cbin, String yAxisLabel, double min,
-      double max, DoubleFunction<Double> function) {
+  public static JFreeChart createHistogram(double[] data, double binwidth, String yAxisLabel,
+      double min, double max, DoubleFunction<Double> function) {
     if (data != null && data.length > 0) {
       double datawidth = (max - min);
-      double binwidth = datawidth / cbin;
+      int cbin = (int) Math.ceil(datawidth / binwidth);
       int[] bins = new int[cbin + 1];
 
       // if value>bin.upper put in next
       for (double v : data) {
-        int i = (int) Math.ceil((v - min) / binwidth);
+        int i = (int) Math.ceil((v - min) / binwidth) - 1;
+        if (i < 0) // does only happen if min>than minimum value of data
+          i = 0;
         bins[i]++;
       }
 
+      // gaussian fit
+      GaussianCurveFitter fitter = GaussianCurveFitter.create();
+      WeightedObservedPoints obs = new WeightedObservedPoints();
+
+      double gMin = 35, gMax = 55;
+      double gWidth = gMax - gMin;
+
+      int sum = 0;
       XYSeries series = new XYSeries("histo", true, true);
       for (int i = 0; i < bins.length; i++) {
         if (bins[i] > 0) {
-          double x = function.apply(min + i * binwidth).doubleValue();
-          double xe = function.apply(min + (i + 1) * binwidth).doubleValue();
+          double x = function.apply(min + (binwidth / 2.0) + i * binwidth).doubleValue();
           series.add(x, bins[i]);
-          // if (xe > x)
-          series.add(xe, bins[i]);
+          sum += bins[i];
+          // add to gaussian
+          if (x >= gMin && x <= gMax)
+            obs.add(x, bins[i]);
+        }
+      }
+
+
+      double[] bestFit = fitter.fit(obs.toList());
+
+      Gaussian g = new Gaussian(bestFit[0], bestFit[1], bestFit[2]);
+
+      XYSeries gs = new XYSeries("Gaussian");
+      int steps = 1000;
+      for (int i = 0; i <= steps; i++) {
+        double x = gMin + (gWidth / steps) * i;
+        double y = normal.value(x);
+        gs.add(x, y);
+      }
+      XYSeriesCollection gsdata = new XYSeriesCollection(gs);
+
+
+      // see when 98% of the data is displayed
+      int sum2 = 0;
+      double barwidth = 0;
+      for (int i = 0; i < bins.length; i++) {
+        if (bins[i] > 0) {
+          sum2 += bins[i];
+          if ((sum2 / (double) sum) >= 0.99) {
+            barwidth = function.apply(min + (binwidth / 2.0) + i * binwidth).doubleValue()
+                - function.apply(min + (binwidth / 2.0) + (i - 1) * binwidth).doubleValue();
+          }
         }
       }
 
       XYSeriesCollection xydata = new XYSeriesCollection(series);
-      XYBarDataset dataset = new XYBarDataset(xydata, binwidth);
+      XYBarDataset dataset = new XYBarDataset(xydata, barwidth);
 
       // HistogramDataset dataset = new HistogramDataset();
       // dataset.addSeries("histo", data, cbin, min, max);
 
       // JFreeChart chart = ChartFactory.createHistogram("", yAxisLabel, "n", dataset,
       // PlotOrientation.VERTICAL, true, false, false);
-      // JFreeChart chart = ChartFactory.createXYBarChart("", yAxisLabel, false, "n", dataset,
-      // PlotOrientation.VERTICAL, true, false, false);
-
-      JFreeChart chart = ChartFactory.createXYLineChart("", yAxisLabel, "n", xydata,
+      JFreeChart chart = ChartFactory.createXYBarChart("", yAxisLabel, false, "n", dataset,
           PlotOrientation.VERTICAL, true, false, false);
+
+      XYPlot xyplot = chart.getXYPlot();
+      // add gaussian
+      xyplot.setDataset(1, gsdata);
+      xyplot.setRenderer(1, new XYLineAndShapeRenderer(true, false));
+
+      // JFreeChart chart = ChartFactory.createXYLineChart("", yAxisLabel, "n", xydata,
+      // PlotOrientation.VERTICAL, true, false, false);
 
 
       chart.setBackgroundPaint(new Color(230, 230, 230));
       chart.getLegend().setVisible(false);
-      XYPlot xyplot = chart.getXYPlot();
       xyplot.setForegroundAlpha(0.7F);
       xyplot.setBackgroundPaint(Color.WHITE);
       xyplot.setDomainGridlinePaint(new Color(150, 150, 150));
       xyplot.setRangeGridlinePaint(new Color(150, 150, 150));
       xyplot.getDomainAxis().setVisible(true);
       xyplot.getRangeAxis().setVisible(yAxisLabel != null);
-      // XYBarRenderer xybarrenderer = (XYBarRenderer) xyplot.getRenderer();
-      // xybarrenderer.setShadowVisible(false);
-      // xybarrenderer.setBarPainter(new StandardXYBarPainter());
-      // xybarrenderer.setDrawBarOutline(false);
+      XYBarRenderer xybarrenderer = (XYBarRenderer) xyplot.getRenderer();
+      xybarrenderer.setShadowVisible(false);
+      xybarrenderer.setBarPainter(new StandardXYBarPainter());
+      xybarrenderer.setDrawBarOutline(false);
       return chart;
     } else
       return null;
@@ -110,8 +158,9 @@ public class EChartFactory {
   }
 
   public static JFreeChart createHistogram(double[] data) {
-    int bin = (int) Math.sqrt(data.length);
-    return createHistogram(data, bin);
+    double bin = Math.sqrt(data.length);
+    Range r = getBounds(data);
+    return createHistogram(data, r.getLength() / bin);
   }
 
   public static JFreeChart createHistogram(double[] data, String yAxisLabel) {
@@ -127,7 +176,8 @@ public class EChartFactory {
   public static JFreeChart createHistogram(double[] data, String yAxisLabel, double min, double max,
       DoubleFunction<Double> function) {
     int bin = (int) Math.sqrt(data.length);
-    return createHistogram(data, bin, yAxisLabel, min, max, function);
+    Range r = getBounds(data);
+    return createHistogram(data, r.getLength() / bin, yAxisLabel, min, max, function);
   }
 
   /**
@@ -154,8 +204,7 @@ public class EChartFactory {
     if (width <= 0)
       return createHistogram(data, yAxisLabel, min, max);
     else {
-      int bin = (int) ((max - min) / width);
-      return createHistogram(data, bin, yAxisLabel, min, max, val -> val);
+      return createHistogram(data, width, yAxisLabel, min, max, val -> val);
     }
   }
 
@@ -172,8 +221,7 @@ public class EChartFactory {
     if (width <= 0)
       return createHistogram(data, yAxisLabel, min, max, function);
     else {
-      int bin = (int) ((max - min) / width);
-      return createHistogram(data, bin, yAxisLabel, min, max, function);
+      return createHistogram(data, width, yAxisLabel, min, max, function);
     }
   }
 
